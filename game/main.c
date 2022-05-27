3bfca322d35fb9632f7e74fe7aa24da10b308c97
Rick Shiguemoto <azrick88@outlook.com>
2022-05-27 00:09:47 -0300

    Added some new data types like PIXEL and PERFORMANCE_DATA, now we can draw specific colors to specific pixels and also we have a way to count how many frames we're outputing to the screen and how much time the frame production is taking. Oh, also added a function to initialize a Pixel, very simple function.

--- game/main.c
+++ game/main.c
@@ -1,42 +1,94 @@
 #include<windows.h>
+#include<stdio.h>
 #include<stdlib.h>
+#include<stdint.h>
 
 #include "main.h"
 
 BOOL g_GameIsRunning = FALSE;
 
-int WinMain(HINSTANCE currentInstanceHandle, HINSTANCE previousInstanceHandle, PSTR commandLine, int windowFlags)
-{
-    int return_value = EXIT_SUCCESS;
+GAMEBITMAP g_GameBackbuffer = { 0 };
 
+GAME_PERFORMANCE_DATA g_PerformanceData = { 0 };
+
+int32_t WinMain(HINSTANCE currentInstanceHandle, HINSTANCE previousInstanceHandle, PSTR commandLine, int32_t windowFlags)
+{
     UNREFERENCED_PARAMETER(currentInstanceHandle);
     UNREFERENCED_PARAMETER(previousInstanceHandle);
     UNREFERENCED_PARAMETER(commandLine);
     UNREFERENCED_PARAMETER(windowFlags);
 
+    int16_t return_value = EXIT_SUCCESS;
+
+    float timeElapsedInMicrosecondsPerFrameSum = 0;
+    float timeElapsedInMicrosecondsPerFrame = 0;
+    float timeElapsedInMilisecondsPerFrame = 0;
+
+    LARGE_INTEGER frameStart;
+    LARGE_INTEGER frameEnd;
+    LARGE_INTEGER fixedFrequency;
+
+    MSG message = { 0 };
+
     if (GameIsRunning() == TRUE) {
-        MessageBoxA(NULL, "Another instance of this program is already running...", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        MessageBoxA(NULL, "Another instance of this program is already running...", "Error!", MESSAGEBOX_ERROR_STYLE);
         return_value = EXIT_FAILURE;
         goto Exit;
     }
 
-    HWND windowHandle = CreateMainWindow(GAME_NAME, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_POSITION_X, WINDOW_POSITION_Y);
+    HWND windowHandle = CreateMainWindow(GAME_NAME, GAME_WIDTH, GAME_HEIGHT, GAME_POSITION_X, GAME_POSITION_Y);
     if (windowHandle == NULL) {
         return_value = EXIT_FAILURE;
         goto Exit;
     }
 
-    g_GameIsRunning = TRUE;
+    QueryPerformanceFrequency(&fixedFrequency);
+    g_PerformanceData.frequency = fixedFrequency.QuadPart;
 
-    MSG message = { 0 };
+    //Global Initialization
+    g_GameIsRunning = TRUE;
+    g_GameBackbuffer.bitMapInfo.bmiHeader.biSize = sizeof(g_GameBackbuffer.bitMapInfo.bmiHeader);
+    g_GameBackbuffer.bitMapInfo.bmiHeader.biWidth = GAME_WIDTH;
+    g_GameBackbuffer.bitMapInfo.bmiHeader.biHeight = GAME_HEIGHT;
+    g_GameBackbuffer.bitMapInfo.bmiHeader.biBitCount = GAME_PIXEL_DEPTH;
+    g_GameBackbuffer.bitMapInfo.bmiHeader.biCompression = BI_RGB;
+    g_GameBackbuffer.bitMapInfo.bmiHeader.biPlanes = 1;
+    g_GameBackbuffer.Memory = VirtualAlloc(NULL, GAME_BACKBUFFER_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    if (g_GameBackbuffer.Memory == NULL) {
+        return_value = EXIT_FAILURE;
+        goto Exit;
+    }
+
+    QueryPerformanceCounter(&frameStart);
 
     while (g_GameIsRunning == TRUE) {
+        
         while (PeekMessageA(&message, windowHandle, 0, 0, PM_REMOVE)) {
             DispatchMessageA(&message);
         }
-        //Process input
+        ProcessInput(windowHandle);
+        RenderGraphics(windowHandle);
 
-        //Render frame
+        QueryPerformanceCounter(&frameEnd);
+        timeElapsedInMicrosecondsPerFrame = (float) (frameEnd.QuadPart - frameStart.QuadPart);
+        timeElapsedInMicrosecondsPerFrame = timeElapsedInMicrosecondsPerFrame * 1000000;
+        timeElapsedInMicrosecondsPerFrame = timeElapsedInMicrosecondsPerFrame / g_PerformanceData.frequency;
+        timeElapsedInMicrosecondsPerFrameSum += timeElapsedInMicrosecondsPerFrame;
+        timeElapsedInMilisecondsPerFrame = timeElapsedInMicrosecondsPerFrame / 1000;
+
+        g_PerformanceData.totalRawFramesRendered++;
+
+        if ((g_PerformanceData.totalRawFramesRendered % FRAMES_TO_CALC_AVERAGE) == 0) {
+            float averageMilisecondsPerFrame = (timeElapsedInMicrosecondsPerFrameSum / FRAMES_TO_CALC_AVERAGE) / 1000;
+
+            char aux_str[256];
+            _snprintf_s(aux_str, _countof(aux_str), _TRUNCATE, "%.3f ms (LAST FRAME) - %.3f ms (AVERAGE)\n", timeElapsedInMilisecondsPerFrame, averageMilisecondsPerFrame);
+            OutputDebugStringA(aux_str);
+
+            timeElapsedInMicrosecondsPerFrameSum = 0;
+        }
+
+        frameStart.QuadPart = frameEnd.QuadPart;
     }
 
 Exit:
@@ -51,7 +103,6 @@ LRESULT CALLBACK MainWndProc(HWND windowHandle, UINT messageID, WPARAM wParamete
     {
         case WM_CLOSE:
         {
-            OutputDebugStringA("WM_CLOSE\n");
             g_GameIsRunning = FALSE;
             PostQuitMessage(0);
             break;
@@ -59,7 +110,6 @@ LRESULT CALLBACK MainWndProc(HWND windowHandle, UINT messageID, WPARAM wParamete
 
         default:
         {
-            OutputDebugStringA("DEFAULT\n");
             result = DefWindowProcA(windowHandle, messageID, wParameter, lParameter);
             break;
         }
@@ -67,9 +117,9 @@ LRESULT CALLBACK MainWndProc(HWND windowHandle, UINT messageID, WPARAM wParamete
     return result;
 }
 
-HWND CreateMainWindow(const char* windowTitle, int width, int height, int windowX, int windowY)
+HWND CreateMainWindow(const char* windowTitle, uint16_t width, uint16_t height, uint16_t windowX, uint16_t windowY)
 {
-    HWND resultHandle = NULL;
+    HWND windowHandle = NULL;
 
     //Window Class creation and registration
     WNDCLASSEXA windowClass = { 0 };
@@ -81,29 +131,62 @@ HWND CreateMainWindow(const char* windowTitle, int width, int height, int window
     windowClass.hInstance = GetModuleHandleA(NULL);
     windowClass.hIcon = LoadIconA(NULL, IDI_APPLICATION);
     windowClass.hCursor = LoadCursorA(NULL, IDC_ARROW);
-    windowClass.hbrBackground = GetStockObject(WHITE_BRUSH);
+    windowClass.hbrBackground = CreateSolidBrush(RGB(255, 0, 255));
     windowClass.lpszMenuName = "MainMenu";
     windowClass.lpszClassName = "MainWindowClass";
 
     //Window Registration
     if (!RegisterClassExA(&windowClass)) {
-        MessageBoxA(NULL, "Error while registering the window class...", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        MessageBoxA(NULL, "Error while registering the window class...", "Error!", MESSAGEBOX_ERROR_STYLE);
         goto Exit;
     }
 
     //Creating first Window Class Handle
-    resultHandle = CreateWindowExA(0, windowClass.lpszClassName, windowTitle, WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+    windowHandle = CreateWindowExA(0, windowClass.lpszClassName, windowTitle, WS_OVERLAPPEDWINDOW | WS_VISIBLE,
                                         windowX, windowY, width, height, NULL, NULL, windowClass.hInstance, NULL);
 
-    if (resultHandle == NULL)
+    if (windowHandle == NULL)
     {
-        MessageBoxA(NULL, "Error while creating the instance of the window class...", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        MessageBoxA(NULL, "Error while creating the instance of the window class...", "Error!", MESSAGEBOX_ERROR_STYLE);
+        goto Exit;
+    }
+    
+    //Getting information from user's monitor
+    g_PerformanceData.monitorInfo.cbSize = sizeof(MONITORINFO);
+    BOOL getMonitorInfoReturn = GetMonitorInfoA(MonitorFromWindow(windowHandle, MONITOR_DEFAULTTOPRIMARY), &g_PerformanceData.monitorInfo);
+    if (getMonitorInfoReturn == 0) {
+        MessageBoxA(NULL, "Error while getting monitor's information...", "Error!", MESSAGEBOX_ERROR_STYLE);
+        
+        //Even if CreateWindowExA succeced, we'll still return NULL.
+        windowHandle = NULL;
+        goto Exit;
+    }
+
+    g_PerformanceData.monitorWidth = g_PerformanceData.monitorInfo.rcMonitor.right - g_PerformanceData.monitorInfo.rcMonitor.left;
+    g_PerformanceData.monitorHeight = g_PerformanceData.monitorInfo.rcMonitor.bottom - g_PerformanceData.monitorInfo.rcMonitor.top;
+
+    LONG_PTR pointerReturned = SetWindowLongPtrA(windowHandle, GWL_STYLE, (WS_OVERLAPPEDWINDOW | WS_VISIBLE) & ~WS_OVERLAPPEDWINDOW);
+    if (pointerReturned == 0) {
+        MessageBoxA(NULL, "Error while changing the window's style...", "Error!", MESSAGEBOX_ERROR_STYLE);
+        
+        //Even if CreateWindowExA succeced, we'll still return NULL.
+        windowHandle = NULL;
+        goto Exit;
+    }
+
+    BOOL setWindowPosReturn = SetWindowPos(windowHandle, HWND_TOPMOST, g_PerformanceData.monitorInfo.rcMonitor.left, g_PerformanceData.monitorInfo.rcMonitor.top, 
+                                                                       g_PerformanceData.monitorWidth/2, g_PerformanceData.monitorHeight/2, SWP_FRAMECHANGED);
+    if (setWindowPosReturn == 0) {
+        MessageBoxA(NULL, "Error while setting window position...", "Error!", MESSAGEBOX_ERROR_STYLE);
+        
+        //Even if CreateWindowExA succeced, we'll still return NULL.
+        windowHandle = NULL;
         goto Exit;
     }
 
 Exit:
 
-    return resultHandle;
+    return windowHandle;
 }
 
 BOOL GameIsRunning(void) {
@@ -114,4 +197,47 @@ BOOL GameIsRunning(void) {
     }else{
         return FALSE;
     }
+}
+
+void ProcessInput(HWND windowHandle) {
+    int16_t escKeyIsDown = GetAsyncKeyState(VK_ESCAPE);
+
+    if (escKeyIsDown) {
+        SendMessageA(windowHandle, WM_CLOSE, 0, 0);
+    }
+}
+
+void RenderGraphics(HWND windowHandle) {
+
+    PIXEL p = InitializePixel(0x00, 0x00, 0x00, 0x00);
+    
+    for (size_t i = 0; i < (GAME_WIDTH * GAME_HEIGHT); i++)
+    {
+        memcpy_s((PIXEL*) g_GameBackbuffer.Memory + i, GAME_BACKBUFFER_SIZE /2, &p, sizeof(PIXEL));
+    }
+
+    HDC deviceContext = GetDC(windowHandle);
+
+    int32_t stretchDIBitsReturn = StretchDIBits(deviceContext, 0, 0, g_PerformanceData.monitorWidth, g_PerformanceData.monitorHeight,
+                                                               0, 0, GAME_WIDTH, GAME_HEIGHT,
+                                                               g_GameBackbuffer.Memory, &g_GameBackbuffer.bitMapInfo, DIB_RGB_COLORS, SRCCOPY);
+    if (stretchDIBitsReturn == 0) {
+        MessageBoxA(NULL, "Error while stretching the backbuffer to the monitor...", "Error!", MESSAGEBOX_ERROR_STYLE);
+        goto Exit;
+    }
+
+Exit: 
+
+    ReleaseDC(windowHandle, deviceContext);
+}
+
+PIXEL InitializePixel(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) {
+    PIXEL pixel = { 0 };
+
+    pixel.red = red;
+    pixel.green = green;
+    pixel.blue = blue;
+    pixel.alpha = alpha;
+    
+    return pixel;
 }
--- game/main.h
+++ game/main.h
@@ -1,12 +1,69 @@
 #pragma once
 
-#define GAME_NAME				"Game"
+#define GAME_NAME					"Game"
 
-#define WINDOW_WIDTH            600
-#define WINDOW_HEIGHT           400
-#define WINDOW_POSITION_X       100
-#define WINDOW_POSITION_Y       100
+#define GAME_WIDTH					384
+#define GAME_HEIGHT					240
+#define GAME_POSITION_X				100
+#define GAME_POSITION_Y				100
+#define GAME_PIXEL_DEPTH			32														//In bits
+#define GAME_BACKBUFFER_SIZE		(GAME_WIDTH * GAME_HEIGHT * (GAME_PIXEL_DEPTH / 8))		//In bytes
+#define BYTES_PER_PIXEL				(GAME_PIXEL_DEPTH / 8)
 
-LRESULT CALLBACK MainWndProc(HWND windowHandle, UINT messageID, WPARAM wParameter, LPARAM lParameter);
-HWND CreateMainWindow(const char* windowTitle, int width, int height, int windowX, int windowY);
-BOOL GameIsRunning(void);
+#define FRAMES_TO_CALC_AVERAGE		100
+
+#define MESSAGEBOX_ERROR_STYLE		(MB_ICONEXCLAMATION | MB_OK)
+
+/// <summary>
+/// 
+/// These are custom data types used to facilitate development and
+///		make it more organized, since I could simply make them all
+///		global variables.
+/// 
+/// </summary>
+
+typedef struct GAMEBITMAP {
+	BITMAPINFO bitMapInfo;							
+	void* Memory;									//Actual memory buffer (although it's a void pointer, we're interpreting this a pixel buffer)
+} GAMEBITMAP;
+
+typedef struct GAME_PERFORMANCE_DATA {
+	int64_t frequency;								//Fixed frequency initialized at boot time
+	
+	uint64_t totalRawFramesRendered;				//Total of actual frames rendered
+
+	uint32_t rawFPSAverage;							//Actual FPS
+	uint16_t virtualFPSAverage;						//FPS perceived by the player
+
+	MONITORINFO monitorInfo;						//Information about the player's monitor
+
+	int32_t monitorWidth;							//Player's monitor width
+	int32_t monitorHeight;							//Player's monitor height
+
+} GAME_PERFORMANCE_DATA;
+
+typedef struct PIXEL {
+	uint8_t blue;
+	uint8_t green;
+	uint8_t red;
+	uint8_t alpha;
+} PIXEL;
+
+/// <summary>
+/// 
+///	These are the declarations of custom functions I used
+///		throughout the development of this project.
+/// 
+/// For now these are not entirely organized in terms of 
+///		what they're used for, I just think of a function
+///		to make and declare it here at the end of the list.
+///		All organize it later.
+/// 
+/// </summary>
+
+LRESULT CALLBACK MainWndProc(HWND windowHandle, UINT messageID, WPARAM wParameter, LPARAM lParameter);							//Responder of window messages
+HWND CreateMainWindow(const char* windowTitle, uint16_t width, uint16_t height, uint16_t windowX, uint16_t windowY);
+BOOL GameIsRunning(void);																										//Function to prevent multiples instances of this same program running simutaneasly
+void ProcessInput(HWND windowHandle);
+void RenderGraphics(HWND windowHandle);
+PIXEL InitializePixel(uint8_t blue, uint8_t green, uint8_t red, uint8_t alpha);
